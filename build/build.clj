6b6b9c96b6057d5a3b65c98d1c27b185bd2843a9
Kiran Shila <me@kiranshila.com>
2021-09-21 17:25:14 -0700

    Switching away from depstar to vanilla tools.build

--- build/build.clj
+++ build/build.clj
@@ -1,17 +1,20 @@
 (ns build
   (:require
-   [clojure.tools.build.api :as b]
-   [hf.depstar :as depstar]))
+   [clojure.tools.build.api :as b]))
 
-(defn uberjar [params]
-  (b/delete {:path "target"})
-  (-> (merge {:jar "Doplarr.jar"
-              :aot true
-              :sync-pom true
-              :target-dir "target"
-              :main-class "doplarr.core"}
-             params)
-      (depstar/aot)
-      (depstar/pom)
-      (assoc :jar-type :uber)
-      (depstar/build)))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def uber-file (str "target/doplarr.jar"))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn uber [_]
+  (clean nil)
+  (b/compile-clj {:basis basis
+                  :src-dirs ["src"]
+                  :class-dir class-dir})
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :main 'doplarr.core
+           :basis basis}))
